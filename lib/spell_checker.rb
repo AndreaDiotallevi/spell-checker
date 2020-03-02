53bd7fb2dbfa16d93f988150b6aa900ef2090a8e
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-02 11:28:58 +0000

    Refactor code to include a constant containing the correct words

--- lib/spell_checker.rb
+++ lib/spell_checker.rb
@@ -1,6 +1,9 @@
+CORRECT_WORDS = ["world", "home"]
+
 def spell_checker(phrase)
-  if phrase == "world" || phrase == "home"
+  if CORRECT_WORDS.include? phrase
     return phrase
   end
+  
   return "~worrd~"
 end
